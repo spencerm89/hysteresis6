73a1f23a410cc7c29d03df06d9519495e7ae6cbc
aparkhurst <aparkhurst@unl.edu>
2015-02-01 14:42:05 -0600

    Update fel.harmonic2.R
    original order c 1st

--- R/fel.harmonic2.R
+++ R/fel.harmonic2.R
@@ -25,10 +25,10 @@ der.summ <- fitstatistics(x,y,newx,newy,n,method="harmonic2",period)
 amps <- derived.amps(b.x,b.y,retention)  
   focus <- derived.focus(inti[3],inti[4],inti[1])
   
-   z <- c( "b.x"=b.x,"b.y"=b.y,"retention"=retention,
-           "lag"=der[2], "coercion"=der[3],"area"=der[1],"phase.angle"=phase.angle, "rote.rad"=inti[1],
+   z <- c( "cx"=cx,"cy"=cy,"b.x"=b.x,"b.y"=b.y,"phase.angle"=phase.angle,"retention"=retention,
+           "area"=der[1],"lag"=der[2], "coercion"=der[3], "rote.rad"=inti[1],
           "rote.deg"=inti[2],"semi.major"=inti[3],"semi.minor"=inti[4],"split.angle"=amps[1],
-          "hysteresis.x"=amps[2],"hysteresis.y"=amps[3],"ampx"=amps[4],"ampy"=amps[5],"cx"=cx,"cy"=cy,
+          "hysteresis.x"=amps[2],"hysteresis.y"=amps[3],"ampx"=amps[4],"ampy"=amps[5],
           "focus.x"=focus[1],"focus.y"=focus[2], "eccentricity"=focus[3])
    res<-list("method"="harmonic2","x"=x,"y"=y,"pred.x"=newx,"pred.y"=newy,"period.time"=ti,
              "values"=z,"fit"=list(Ta.lm,Tb.lm),"fit.statistics"=der.summ,"residuals"=residual)
